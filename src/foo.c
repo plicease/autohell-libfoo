63c249f39c72e67eceb1e38199a08b39c9d14b6e
Graham Ollis <plicease@cpan.org>
2014-05-07 18:18:56 -0400

    un working dll stuffish

--- src/foo.c
+++ src/foo.c
@@ -1,19 +1,31 @@
+#if HAVE_CONFIG_H
+# include <config.h>
+#endif
+
 #include "foo.h"
 
-const char *
+FOO_EXPORT const char *
 foo_name()
 {
   return "libfoo";
 }
 
-const char *
+FOO_EXPORT const char *
 foo_version_string()
 {
   return "1.00";
 }
 
-int
+FOO_EXPORT int
 foo_version_int()
 {
   return 100;
 }
+
+#if defined WIN32 && defined DLL_EXPORT
+char
+libfoo_is_dll(void)
+{
+  return 1;
+}
+#endif
--- src/foo.h
+++ src/foo.h
@@ -1,8 +1,20 @@
 #ifndef FOO_H
 #define FOO_H
 
-const char *foo_name();
-const char *foo_version_string();
-int foo_version_int();
+#ifdef _WIN32
+# ifdef DLL_EXPORT
+#  define FOO_EXPORT __declspec(dllexport)
+# else
+#  ifdef LIBFOO_DLL_IMPORT
+#   define FOO_EXPORT extern __declspec(dllimport)
+#  endif
+# endif
+#else
+# define FOO_EXPORT extern
+#endif
+
+FOO_EXPORT const char *foo_name();
+FOO_EXPORT const char *foo_version_string();
+FOO_EXPORT int foo_version_int();
 
 #endif
